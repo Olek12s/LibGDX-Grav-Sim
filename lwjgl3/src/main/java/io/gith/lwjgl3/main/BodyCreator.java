3d26471ccd3a8212da3924ae3cd55a9a94d4277a
Oleki <olekignaczak78@gmail.com>
2025-09-22 01:26:05 +0200

    spawning galaxy added

--- lwjgl3/src/main/java/io/gith/lwjgl3/main/BodyCreator.java
+++ lwjgl3/src/main/java/io/gith/lwjgl3/main/BodyCreator.java
@@ -25,6 +25,7 @@ public class BodyCreator implements Updatable {
 
         if (ImGui.getIO().getWantCaptureMouse()) return;    // ignore click if ImGui already captured it
 
+        // LEFT CLICK
         if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT))
         {
             Vector2 worldPos = cameraController.screenToWorld(Gdx.input.getX(), Gdx.input.getY());
@@ -45,10 +46,43 @@ public class BodyCreator implements Updatable {
                 }
             }
         }
+
+        // MIDDLE CLICK
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.MIDDLE)) {
+            Vector2 worldPos = cameraController.screenToWorld(Gdx.input.getX(), Gdx.input.getY());
+            addGalaxy(worldPos, 30_000, 100_000_000f);
+        }
     }
 
     private void addBodyAt(Vector2 position, Vector2 velocity, float mass, Color color) {
         Body body = new Body(position, velocity, mass, Color.LIME);
         quadTree.addNewBody(body);
     }
+
+    private void addGalaxy(Vector2 center, int starCount, float totalMass) {
+        Body core = new Body(new Vector2(center), new Vector2(0, 0), totalMass, Color.YELLOW);
+        quadTree.addNewBody(core);
+
+
+        float starMass = 1f;
+        java.util.Random rng = new java.util.Random();
+
+        for (int i = 0; i < starCount; i++) {
+
+            float r = (float) (rng.nextFloat() * 200 + 50); // promień 50–250 (dopasuj)
+            float angle = (float) (rng.nextFloat() * Math.PI * 2);
+            Vector2 pos = new Vector2(
+                center.x + (float)Math.cos(angle) * r,
+                center.y + (float)Math.sin(angle) * r
+            );
+
+
+            float vMag = (float)Math.sqrt(Units.G * totalMass / r);
+
+            Vector2 vel = new Vector2(-(float)Math.sin(angle), (float)Math.cos(angle)).scl(vMag);
+
+            addBodyAt(pos, vel, starMass, Color.WHITE);
+        }
+    }
+
 }
